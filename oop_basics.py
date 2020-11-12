a5d11bbfdc42d503fe618edc7c2c0236bbc310e1
k4i5er <kaiser@kaiser-hw01.localdomain>
2020-11-12 09:19:17 -0600

    Added second class Person

--- oop_basics.py
+++ oop_basics.py
@@ -1,4 +1,6 @@
 class Mandarina():
+
+    energy_amount = 7
     size = 0
     color_types = ('verde', 'naranja', 'oscuro')
     color = ''
@@ -24,35 +26,103 @@ class Mandarina():
     number_of_segments = 0
     size_of_segments = 0
 
-    def rot():
-        state = state_types[2]
-        odor = odor_types[3]
+    # Método 'sucio'
+    @classmethod
+    def rot(cls):
+        cls.state = cls.state_types[2]
+        cls.odor = cls.odor_types[3]
+        cls.weight = 7.7
+        cls.size_of_segments = cls.weight * cls.volume
 
-    def show_props():
+    # Método 'fancy' (elegante)
+    @classmethod
+    def show_props(cls):
         print(f'''
-        tamaño = {size}
-        color = {color}
-        estado = {state}
-        vitaminas = {vitamins}
-        minerales = {minerals}
-        tipo = {object_type}
-        tiempo de nacimiento = {birth_time}
-        olor = {odor}
-        forma = {shape}
-        tipo de planta = {type_of}
-        número de semillas = {seeds}
-        peso = {weight}
-        volumen = {volume}
-        masa = {mass}
-        grosor de cáscara = {peel_thickness}
-        número de gajos = {number_of_segments}
-        tamaño de gajos = {size_of_segments}
+        tamaño = {cls.size}
+        color = {cls.color}
+        estado = {cls.state}
+        vitaminas = {cls.vitamins}
+        minerales = {cls.minerals}
+        tipo = {cls.object_type}
+        tiempo de nacimiento = {cls.birth_time}
+        olor = {cls.odor}
+        forma = {cls.shape}
+        tipo de planta = {cls.type_of}
+        número de semillas = {cls.seeds}
+        peso = {cls.weight}
+        volumen = {cls.volume}
+        masa = {cls.mass}
+        grosor de cáscara = {cls.peel_thickness}
+        número de gajos = {cls.number_of_segments}
+        tamaño de gajos = {cls.size_of_segments}
         ''')
+
+    @classmethod
+    def get_energy(cls):
+        return cls.energy_amount
+
+
+class Person():
+    energy = 0
+    name = ''
+
+    # Método constructor
+    def __init__(self, name='Sin nombre'):
+        self.name = name
+        self.energy = 1
+
+    def set_energy(self, from_who):
+        self.energy = self.energy + from_who
+        # cls.energy = from_who
+
+    def show_props(self):
+        print(f'''
+        Nombre: {self.name}
+        Energía: {self.energy}
+        ''')
+
+    def set_name(self, name):
+        self.name = name
 
 
 # Creo una instancia de la clase Mandarina
-mandi = Mandarina()
+mandi = Mandarina()  # Instancia/Objeto
+eduardo = Person('Eduardo')
+# eduardo.set_name('Eduardo')
+gilberto = Person('Gilberto')
+# gilberto.set_name('Gilberto')
+bibiana = Person('Bibiana')
+# bibiana.set_name('Bibiana')
+otro = Person()
+
+a = 0  # Variable
 
 mandi.show_props()
-mandi.rot()
-mandi.show_props()
+# eduardo.show_props()
+# mandi.rot()
+# mandi.show_props()
+eduardo.set_energy(mandi.get_energy())
+gilberto.set_energy(mandi.get_energy())
+eduardo.show_props()
+gilberto.show_props()
+eduardo.set_energy(mandi.get_energy())
+gilberto.set_energy(mandi.get_energy())
+eduardo.show_props()
+gilberto.show_props()
+eduardo.set_energy(mandi.get_energy())
+gilberto.set_energy(mandi.get_energy())
+eduardo.show_props()
+gilberto.show_props()
+gilberto.set_energy(mandi.get_energy())
+eduardo.show_props()
+gilberto.show_props()
+bibiana.show_props()
+otro.show_props()
+otro.set_name('Ya tengo nombre')
+otro.show_props()
+
+# Funcionalidades para implementar a la clase Person:
+# - Caminar un número de pasos (hacia adelante o atrás)
+# - Correr
+# - Sentarse
+# - Pararse
